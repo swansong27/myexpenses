0e4d8aaaf693a68a42a8f477ebc47e34c3be7756
lebedevim <lebedevim@mosreg.ru>
2018-01-23 15:56:00 +0300

    Initial commit (doesn't start, DB troubles)

---
+++ src/main/java/com/skinnynerd/myexpenses/MyexpensesApplication.java
@@ -0,0 +1,12 @@
+package com.skinnynerd.myexpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyexpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyexpensesApplication.class, args);
+	}
+}
